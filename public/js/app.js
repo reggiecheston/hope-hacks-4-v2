691330a69212d4fa5bce7532ab3bd8d8347160af
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2023-12-18 16:13:37 -0500

    updates public/app.js functionality

--- public/js/app.js
+++ public/js/app.js
@@ -52,41 +52,38 @@ movieForm.addEventListener("submit", async (e) => {
             </div>`
           )
         );
-      }
-    });
-  });
-});
 
-const ctaBtn = document.querySelectorAll(".cta-btn");
+        const ctaBtn = document.querySelectorAll(".cta-btn");
+        console.log(ctaBtn);
 
-ctaBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const id = btn.dataset.movieId;
-    console.log(id);
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${APIKEY}`
-    ).then((response) => {
-      response.json().then((data) => {
-        if (data.total_results === 0) {
-          searchResults.textContent = "";
-          searchResults.insertAdjacentHTML(
-            "afterbegin",
-            `<div class="results-count">
+        ctaBtn.forEach((btn) => {
+          btn.addEventListener("click", () => {
+            const id = btn.dataset.movieId;
+            console.log(id);
+            fetch(
+              `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${APIKEY}`
+            ).then((response) => {
+              response.json().then((data) => {
+                if (data.total_results === 0) {
+                  searchResults.textContent = "";
+                  searchResults.insertAdjacentHTML(
+                    "afterbegin",
+                    `<div class="results-count">
                 <small>Your search didn't match any movies.</small>
             </div>`
-          );
-        } else {
-          searchResults.textContent = "";
-          searchResults.insertAdjacentHTML(
-            "afterbegin",
-            `<div class="results-count">
+                  );
+                } else {
+                  searchResults.textContent = "";
+                  searchResults.insertAdjacentHTML(
+                    "afterbegin",
+                    `<div class="results-count">
                             <small>${data.results.length} results</small>
                         </div>`
-          );
-          data.results.forEach((m) =>
-            searchResults.insertAdjacentHTML(
-              "beforeend",
-              `<div class="search-result">
+                  );
+                  data.results.forEach((m) =>
+                    searchResults.insertAdjacentHTML(
+                      "beforeend",
+                      `<div class="search-result">
                     <img src="https://image.tmdb.org/t/p/w300/${m.poster_path}" alt="${m.original_title} poster">
                     <div class="movie-details">
                         <h3 class="movie-title">${m.original_title}</h3>
@@ -96,10 +93,14 @@ ctaBtn.forEach((btn) => {
                         </div>
                     </div>
                   </div>`
-            )
-          );
-        }
-      });
+                    )
+                  );
+                }
+              });
+            });
+          });
+        });
+      }
     });
   });
 });
