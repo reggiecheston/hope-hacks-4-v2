c2e2051f22d402efcc4576ef5992edccb4a332ab
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2023-12-17 21:59:14 -0500

    updates styling and fixes error handling for search function

--- public/js/app.js
+++ public/js/app.js
@@ -20,20 +20,32 @@ movieForm.addEventListener("submit", async (e) => {
     )}&api_key=${APIKEY}`
   ).then((response) => {
     response.json().then((data) => {
-      if (data.error) {
-        searchResults.innerHTML = `<h2>${data.error}</>`;
+      if (data.total_results === 0) {
+        searchResults.textContent = "";
+        searchResults.insertAdjacentHTML(
+          "afterbegin",
+          `<div class="results-count">
+                    <small>Your search didn't match any movies.</small>
+                </div>`
+        );
       } else {
         searchResults.textContent = "";
+        searchResults.insertAdjacentHTML(
+          "afterbegin",
+          `<div class="results-count">
+                  <small>${data.results.length} results</small>
+              </div>`
+        );
         data.results.forEach((m) =>
           searchResults.insertAdjacentHTML(
             "beforeend",
             `<div class="search-result">
-                <img src="https://image.tmdb.org/t/p/w300/${m.poster_path}" alt="${m.original_title} poster">
-                <div class="movie-details">
-                    <h3 class="movie-title">${m.original_title}</h3>
-                    <p class="movie-overview">${m.overview}</p>
-                    <button>More Info</button>
-              </div>`
+                      <img src="https://image.tmdb.org/t/p/w300/${m.poster_path}" alt="${m.original_title} poster">
+                      <div class="movie-details">
+                          <h3 class="movie-title">${m.original_title}</h3>
+                          <p class="movie-overview">${m.overview}</p>
+                          <button>More Info</button>
+                    </div>`
           )
         );
       }
